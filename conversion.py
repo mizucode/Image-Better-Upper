0dc23ff74be2465281ff0a09ec636455e103d2c3
mizucode <soph.mizucode@gmail.com>
2021-06-10 17:58:28 -0400

    commit soph

---
+++ conversion.py
@@ -0,0 +1,19 @@
+import imageio
+from numpy import asarray
+from PIL import Image
+
+
+def filename():
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+pic = imageio.imread(filename)
+
+array = asarray(pic)
+arraycopy = array.copy()
+
+img = Image.fromarray(arraycopy, 'RGB')
+
+
+img.save(filename)
+img.show()

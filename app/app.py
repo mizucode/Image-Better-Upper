c5412e3db1e3182655643e18309f88b762e88348
mizucode <soph.mizucode@gmail.com>
2021-10-01 20:57:55 -0700

    port addition for heroku port bind fix

--- app/app.py
+++ app/app.py
@@ -114,4 +114,5 @@ def clean_files():
 
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000, url_scheme='https')
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host='0.0.0.0', port=port, url_scheme='https')
